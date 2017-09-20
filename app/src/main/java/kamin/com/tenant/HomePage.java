1fe35fdf9f51d4b7e51f648a2af1805d048bb758
Kammin <ibayka@gmail.com>
2017-09-20 21:47:56 +0300

    add exit on homepage

--- app/src/main/java/kamin/com/tenant/HomePage.java
+++ app/src/main/java/kamin/com/tenant/HomePage.java
@@ -8,19 +8,37 @@ import android.view.View;
 import android.widget.ImageButton;
 
 public class HomePage extends AppCompatActivity {
-    ImageButton ibTenant;
+    ImageButton ibTenant, ibExit;
     Context context;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        int id = android.os.Process.myPid();
+        android.os.Process.killProcess(id);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (getIntent().getExtras() != null && getIntent().getExtras().getBoolean("EXIT", false)) {
+            finish();
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         context = getApplicationContext();
         ibTenant = (ImageButton) findViewById(R.id.ibTenant);
+        ibExit = (ImageButton) findViewById(R.id.ibExit);
         ibTenant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(context, TenantPage.class);
                 startActivity(i);
+            }
+        });
+        ibExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
         VolleySingleton.getInstance(this);
